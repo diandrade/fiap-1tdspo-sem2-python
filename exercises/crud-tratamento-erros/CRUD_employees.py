bb301ce4db99f73b94322e1665c011ee8c868316
diandrade <contato.andradediego@gmail.com>
2025-08-08 17:10:07 -0300

    Ex01 finalizado.

--- exercises/crud-tratamento-erros/CRUD_employees.py
+++ exercises/crud-tratamento-erros/CRUD_employees.py
@@ -4,20 +4,20 @@ display_continue= f'''{separator}
 Do you want to continue?
 1 - Yes.
 0 - No.
-{separator}
 '''
 display_key = f'''Inform the key that you want to change: 
 1 - Code
 2 - Name
 3 - Age
 4 - Salary
-'''
+{separator}'''
 employees = []
 
 def main():
     while True:
         display_menu()
-        choice_option = int(input("\nSelect your option: "))
+        choice_option = int(input("Select your option: "))
+        print(separator)
         while True:
             match choice_option:
                 case 1:
@@ -30,7 +30,8 @@ def main():
                         print("You need to prompt a number to code, age and salary.")
                     else:
                         register_employee(code, name, age, salary)
-                        print("Employee register with succeed")
+                        print(separator)
+                        print("Employee registered successful.")
                     break
                 case 2:
                     try:
@@ -48,18 +49,26 @@ def main():
                         print("You need to prompt a number to code and key")
                     else:
                         update_employee(code, key)
+                        print(separator)
+                        print("Employee updated successful.")
                     break
                 case 4:
-                    print(" Something. ")
+                    try:
+                        code = int(input("Prompt the code of the employee that you want to delete: "))
+                    except ValueError:
+                        print("You need to prompt a number to code.")
+                    else:
+                        delete_employee(code)
+                        print("Employee deleted successful.")
                     break
                 case 5:
-                    print(" Something. ")
+                    read_all_employees()
                     break
                 case 6:
-                    print(" Something. ")
+                    age_superior_than_25()
                     break
                 case 7:
-                    print(" Something. ")
+                    age_between_22_25_and_salary_above_3700()
                     break
                 case _:
                     print(display_wrong_option)
@@ -88,7 +97,57 @@ def read_employee(code):
         if employee['Code'] == code:
             for key, value in employee.items():
                 print(f"{key}: {value}")
+            return
         print(separator)
+    print("Prompt an existent code.")
+
+def update_employee(code, key):
+    options = {
+        1 : 'Code',
+        2 : 'Name',
+        3 : 'Age',
+        4 : 'Salary'
+    }
+    display_input = f"Prompt the new value of {options[key]}: "
+
+    for employee in employees:
+        if employee['Code'] == code:
+            print(f"The oldest value of {options[key]} was {employee[options[key]]}.")
+            if key == 2:
+                new_value = input(display_input)
+                employee[options[key]] = new_value
+                break
+            new_value = int(input(display_input))
+            employee[options[key]] = new_value
+            break
+
+def delete_employee(code):
+    i = 0
+    for employee in employees:
+        if employee['Code'] == code:
+            employees.pop(i)
+            break
+        i = i + 1
+
+def read_all_employees():
+    for employee in employees:
+        for key, value in employee.items():
+            print(f"{key}: {value}")
+        print(separator)
+
+def age_superior_than_25():
+    for employee in employees:
+        if employee['Age'] > 25:
+            for key, value in employee.items():
+                print(f"{key}: {value}")
+            print(separator)
+
+def age_between_22_25_and_salary_above_3700():
+    for employee in employees:
+        if 22 < employee['Age'] < 25 and employee['Salary'] > 3700:
+            for key, value in employee.items():
+                print(f"{key}: {value}")
+            print(separator)
 
 def display_menu():
     menu_options = f'''{separator}
@@ -102,12 +161,6 @@ def display_menu():
 {separator}'''
     print(menu_options)
 
-'''def update_employee(code, key):
-    for employee in employees:
-        if employee['Code'] == code:
-            print(f"The oldest value of {key} was {employee['Code']}.")
-            new_value = int(input())
-'''
 
 if __name__ == '__main__':
     main()
